382313799e8da23b558e89440ac66439c9563274
aTs-source <71322174+aTs-source@users.noreply.github.com>
2023-12-02 17:00:03 +0530

    serach flight page

--- src/pages/search-flight/index.js
+++ src/pages/search-flight/index.js
@@ -13,6 +13,7 @@ import ListItemText from "@mui/material/ListItemText";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import Popover from "@mui/material/Popover";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
@@ -22,6 +23,12 @@ import SyncAltIcon from "@mui/icons-material/SyncAlt";
 import HistoryToggleOffRoundedIcon from "@mui/icons-material/HistoryToggleOffRounded";
 import CorporateFareRoundedIcon from "@mui/icons-material/CorporateFareRounded";
 import SendRoundedIcon from "@mui/icons-material/SendRounded";
+import PersonIcon from "@mui/icons-material/Person";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import FmdGoodIcon from "@mui/icons-material/FmdGood";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+
 import { useDispatch } from "react-redux";
 import { logout } from "@/redux/reducers/userSlice";
 import { useRouter } from "next/router";
@@ -51,6 +58,23 @@ const Item = styled(Paper)(({ theme }) => ({
 function DrawerAppBar(props) {
   const { window } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const [anchorEl_1, setAnchorEl_1] = React.useState(null);
+  const [travellersCount, setTravellersCount] = React.useState(0);
+  const [openTraveller, setopentraveller] = React.useState(false);
+
+  const handleClick_traveller = (event, updater) => {
+    updater(event.currentTarget);
+    // setopentraveller()
+  };
+
+  const handleClose_traveller = (updater) => {
+    updater(null);
+  };
+
+  const open = Boolean(anchorEl_1);
+  const id = open ? "simple-popover" : undefined;
+
   const dispatch = useDispatch();
   const router = useRouter();
 
@@ -89,7 +113,9 @@ function DrawerAppBar(props) {
     window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: "flex" }}>
+    <Box
+      sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
+    >
       <CssBaseline />
       <AppBar component="nav">
         <Toolbar>
@@ -144,77 +170,272 @@ function DrawerAppBar(props) {
       </nav>
       <Box
         component="main"
-        sx={{ p: 3 }}
+        sx={{ p: 10 }}
         style={{
-          width: "80%",
+          width: "95%",
           alignSelf: "center",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
         }}
       >
         <Toolbar />
         {/* Content will be there */}
-        <Button
-          style={{ marginRight: 12, marginBottom: 24, borderRadius: 24 }}
-          variant="outlined"
-          key="one"
-        >
-          <SendRoundedIcon style={{ marginRight: 8 }} />
-          One Day
-        </Button>
-        <Button
-          style={{ marginRight: 12, marginBottom: 24, borderRadius: 24 }}
-          variant="outlined"
-          key="two"
-        >
-          <HistoryToggleOffRoundedIcon style={{ marginRight: 8 }} />
-          Round Trip
-        </Button>
-        <Button
-          style={{ marginRight: 12, marginBottom: 24, borderRadius: 24 }}
-          variant="outlined"
-          key="three"
-        >
-          <CorporateFareRoundedIcon style={{ marginRight: 8 }} />
-          Multi-City
-        </Button>
         <Card sx={{ minWidth: 275 }}>
-          <CardContent>
+          <CardContent
+            style={{
+              backgroundColor: "#121212",
+              justifyContent: "flex-start",
+              display: "flex",
+            }}
+          >
             <Box sx={{ flexGrow: 1 }}>
               <Grid container spacing={2}>
-                <Grid item xs={4}>
-                  <Typography variant="subtitle2">Flight From</Typography>
-                  <Item>xs=4</Item>
-                </Grid>
-                <Grid item xs={1}>
-                  <IconButton
-                    color="inherit"
-                    aria-label="open drawer"
-                    edge="start"
-                    onClick={handleDrawerToggle}
+                <Grid item>
+                  <Item
+                    style={{
+                      borderRadius: 24,
+                      display: "flex",
+                      justifyContent: "space-arround",
+                      alignItems: "center",
+                    }}
                   >
-                    <SyncAltIcon />
-                  </IconButton>
+                    <SendRoundedIcon
+                      style={{
+                        marginRight: 8,
+                        height: 20,
+                        width: 20,
+                        display: "flex",
+                      }}
+                    />
+                    <Typography style={{ display: "flex" }} variant="subtitle3">
+                      One Way
+                    </Typography>
+                  </Item>
                 </Grid>
-                <Grid item xs={4}>
-                  <Typography variant="subtitle2">Flying To</Typography>
-                  <Item>xs=4</Item>
+                <Grid item>
+                  <Item
+                    style={{
+                      borderRadius: 24,
+                      display: "flex",
+                      justifyContent: "space-arround",
+                      alignItems: "center",
+                    }}
+                  >
+                    <HistoryToggleOffRoundedIcon
+                      style={{
+                        marginRight: 8,
+                        height: 20,
+                        width: 20,
+                        display: "flex",
+                      }}
+                    />
+                    <Typography variant="subtitle2">Round Trip</Typography>
+                  </Item>
                 </Grid>
-                <Grid item xs={3}>
-                  <Typography variant="subtitle2">Departure Date</Typography>
-                  <Item>xs=4</Item>
-                </Grid>
-                <Grid item xs={4}>
-                  <Typography variant="subtitle2">Traveller(s)</Typography>
-                  <Item>xs=8</Item>
-                </Grid>
-                <Grid item xs={4}>
-                  <Typography variant="subtitle2">Preferred Class</Typography>
-                  <Item>xs=8</Item>
+                <Grid item>
+                  <Item
+                    style={{
+                      borderRadius: 24,
+                      display: "flex",
+                      justifyContent: "space-arround",
+                      alignItems: "center",
+                    }}
+                  >
+                    <CorporateFareRoundedIcon style={{ marginRight: 8 }} />
+                    <Typography variant="subtitle2">Multi-City</Typography>
+                  </Item>
                 </Grid>
               </Grid>
             </Box>
           </CardContent>
-          <CardActions>
-            <Button size="small" variant="contained">
+        </Card>
+
+        <Card sx={{ minWidth: 275, padding: 2 }}>
+          <CardContent>
+            <Box sx={{ flexGrow: 1 }}>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={4} lg={4}>
+                  <Typography variant="subtitle2">Flight From</Typography>
+                  <Item
+                    style={{
+                      justifyContent: "space-between",
+                      display: "flex",
+                      alignItems: "center",
+                    }}
+                  >
+                    {/* <PersonIcon style={{ marginRight: 8 }} /> */}
+                    <Typography variant="subtitle3">
+                      1 Adults . 0 Child . 0 Infant
+                    </Typography>
+                    <FmdGoodIcon style={{ marginLeft: 8 }} />
+                  </Item>
+                </Grid>
+                <Grid
+                  item
+                  xs={4}
+                  sm={1}
+                  lg={1}
+                  style={{
+                    justifyContent: "center",
+                    display: "flex",
+                    alignItems: "flex-end",
+                    padding: 12,
+                  }}
+                >
+                  <SyncAltIcon style={{ display: "flex" }} />
+                </Grid>
+                <Grid item xs={12} sm={4} lg={4}>
+                  <Typography variant="subtitle2">Flying To</Typography>
+                  <Item
+                    style={{
+                      justifyContent: "space-between",
+                      display: "flex",
+                      alignItems: "center",
+                    }}
+                  >
+                    <PersonIcon style={{ marginRight: 8 }} />
+                    <Typography variant="subtitle3">
+                      1 Adults . 0 Child . 0 Infant
+                    </Typography>
+                    <ExpandMoreIcon style={{ marginLeft: 8 }} />
+                  </Item>
+                </Grid>
+                <Grid item xs={12} sm={3} lg={3}>
+                  <Typography variant="subtitle2">Departure Date</Typography>
+                  <Item
+                    style={{
+                      justifyContent: "space-between",
+                      display: "flex",
+                      alignItems: "center",
+                    }}
+                  >
+                    <PersonIcon style={{ marginRight: 8 }} />
+                    <Typography variant="subtitle3">
+                      1 Adults . 0 Child . 0 Infant
+                    </Typography>
+                    <ExpandMoreIcon style={{ marginLeft: 8 }} />
+                  </Item>
+                </Grid>
+                <Grid item xs={12} sm={4} lg={4}>
+                  <Typography variant="subtitle2">Traveller(s)</Typography>
+                  <Item
+                    style={{
+                      justifyContent: "space-between",
+                      display: "flex",
+                      alignItems: "center",
+                    }}
+                    onClick={(event) =>
+                      handleClick_traveller(event, setAnchorEl_1)
+                    }
+                  >
+                    <PersonIcon style={{ marginRight: 8 }} />
+                    <Typography variant="subtitle3">
+                      {travellersCount} Adults . 0 Child . 0 Infant
+                    </Typography>
+                    <ExpandMoreIcon style={{ marginLeft: 8 }} />
+                  </Item>
+                  <Popover
+                    id={"simple-popover"}
+                    open={open}
+                    anchorEl={anchorEl_1}
+                    onClose={() => handleClose_traveller(setAnchorEl_1)}
+                    anchorOrigin={{
+                      vertical: "bottom",
+                      horizontal: "left",
+                    }}
+                  >
+                    <Typography sx={{ p: 2 }} variant="subtitle2">
+                      Traveller(s)
+                    </Typography>
+                    <div
+                      style={{
+                        flexDirection: "row",
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        flexDirection: "row",
+                      }}
+                    >
+                      <Typography sx={{ p: 2 }} variant="subtitle2">
+                        Adult
+                      </Typography>
+                      <div
+                        style={{
+                          flexDirection: "row",
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "center",
+                        }}
+                      >
+                        <Button
+                          style={{
+                            height: 28,
+                            width: 10,
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                          }}
+                          variant="contained"
+                          disabled={travellersCount <= 0}
+                          onClick={() => setTravellersCount((prev) => prev - 1)}
+                        >
+                          <RemoveIcon />
+                        </Button>
+                        <Typography sx={{ p: 2 }} variant="subtitle2">
+                          {travellersCount}
+                        </Typography>
+                        <Button
+                          style={{
+                            height: 28,
+                            width: 10,
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                          }}
+                          variant="contained"
+                          onClick={() => setTravellersCount((prev) => prev + 1)}
+                        >
+                          <AddIcon />
+                        </Button>
+                      </div>
+                      <Divider />
+                    </div>
+                  </Popover>
+                </Grid>
+                <Grid item xs={12} sm={4} lg={4}>
+                  <Typography variant="subtitle2">Preferred Class</Typography>
+                  <Item
+                    style={{
+                      justifyContent: "space-between",
+                      display: "flex",
+                      alignItems: "center",
+                    }}
+                  >
+                    <PersonIcon style={{ marginRight: 8 }} />
+                    <Typography variant="subtitle3">
+                      1 Adults . 0 Child . 0 Infant
+                    </Typography>
+                    <ExpandMoreIcon style={{ marginLeft: 8 }} />
+                  </Item>
+                </Grid>
+              </Grid>
+            </Box>
+          </CardContent>
+          <CardActions
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+              display: "flex",
+              marginTop: 24,
+            }}
+          >
+            <Button
+              size="small"
+              variant="contained"
+              style={{ display: "flex" }}
+            >
               Submit
             </Button>
           </CardActions>
